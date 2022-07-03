f83662560d16187a32d8cc3a294c503eb9cd85e4
mishasomov <somovmykhailo@gmail.com>
2022-07-03 16:58:22 +0200

    Refactored to use commands for basic actions and page verification

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -4,39 +4,26 @@ import alertText from '../../fixtures/alertstext.json'
 
 describe('Login', () => {
 
-  beforeEach(function() {
-    cy.visit('/');
-  })
+    beforeEach(function () {
+        cy.visit('/');
+    })
 
-  it('Registered user is able to login', () => {
-    cy.get('#inputEmail').type(userData.userEmail);
-    cy.get('#inputPassword').type(userData.userPassword);
-    cy.contains('Login').click();
-    cy.url().should('include', '/account/dashboard')
+    it('Registered user is able to login', () => {
+        cy.login(userData.userEmail, userData.userPassword)
+        cy.verifyDashboardPageIsOpened(userData.userName)
+    })
 
-    cy.get('[data-ride="carousel"]').should('be.visible');
-    cy.get('a.admin-dropdown.profile').should('contain.text', 'Misha Test')
-    cy.get('a.cta-main').should('be.visible')
-    cy.get('div.nav-bar-right.clearfix').should('be.visible')
-    cy.get('a.account-logo').should('be.visible')
-    cy.get('input#focus-new.search').should('be.visible')
-  })
+    it('User is be able to Logout', () => {
+        cy.login(userData.userEmail, userData.userPassword)
+        cy.logout()
+        cy.get('p.cbox_messagebox').should('contain.text', alertText.youHaveSuccessfullyLoggedOut)
+        cy.verifyLoginPageIsOpened()
+    })
 
-  it('User is be able to Logout', () => {
-    cy.login(userData.userEmail, userData.userPassword)
-    cy.get('a.admin-dropdown.profile').click()
-    cy.contains('Logout').click()
-    cy.get('p.cbox_messagebox')
-    cy.get('#inputEmail').should('be.visible')
-    cy.get('#inputPassword').should('be.visible')
-    cy.contains('Login').should('be.visible')
-    cy.get('a.admin-bar-logo').should('be.visible')
-  })
-
-  it('Non-existing User is not able to Login', () => {
-    cy.login(userData.incorrectUserEmail, userData.incorrectUserPassword)
-    cy.get('div.cbox_messagebox_error').should('be.visible')
-    cy.contains(alertText.incorrectUserNameOrPassword).should('be.visible')
-    cy.url().should('include', '/login')
-  })
+    it('Non-existing User is not able to Login', () => {
+        cy.login(userData.incorrectUserEmail, userData.incorrectUserPassword)
+        cy.get('div.cbox_messagebox_error').should('be.visible')
+        cy.contains(alertText.incorrectUserNameOrPassword).should('be.visible')
+        cy.url().should('include', '/login')
+    })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,28 @@
-Cypress.Commands.add('login', (username, password) => {
-     cy.get('#inputEmail').type(username);
-     cy.get('#inputPassword').type(password);
-     cy.contains('Login').click();
+Cypress.Commands.add('login', (userEmail, password) => {
+    cy.get('#inputEmail').type(userEmail);
+    cy.get('#inputPassword').type(password);
+    cy.contains('Login').click();
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('a.admin-dropdown.profile').click()
+    cy.contains('Logout').click()
+})
+
+Cypress.Commands.add('verifyDashboardPageIsOpened', (userName) => {
+    cy.url().should('include', '/account/dashboard')
+    cy.get('[data-ride="carousel"]').should('be.visible');
+    cy.get('a.admin-dropdown.profile').should('contain.text', userName)
+    cy.get('a.cta-main').should('be.visible')
+    cy.get('div.nav-bar-right.clearfix').should('be.visible')
+    cy.get('a.account-logo').should('be.visible')
+    cy.get('input#focus-new.search').should('be.visible')
+})
+
+Cypress.Commands.add('verifyLoginPageIsOpened', () => {
+    cy.get('#inputEmail').should('be.visible')
+    cy.get('#inputPassword').should('be.visible')
+    cy.contains('Login').should('be.visible')
+    cy.get('a.admin-bar-logo').should('be.visible')
+    cy.url().should('include', '/login')
 })
